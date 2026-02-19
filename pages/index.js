a9ecf2e054fcb8ce8c54289f208113b2504b3619
Victor Lucas Moreira Santos <victor.moreira@atakarejo.com.br>
2026-02-19 18:19:30 -0300

    fix rota raiz

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,12 @@ export default function Home() {
   return null
 }
 
-export async function getServerSideProps() {
-  return {
-    redirect: {
-      destination: '/index.html',
-      permanent: false,
-    },
-  }
+export async function getServerSideProps({ res }) {
+  res.setHeader('Content-Type', 'text/html')
+  const fs = require('fs')
+  const path = require('path')
+  const html = fs.readFileSync(path.join(process.cwd(), 'public', 'index.html'), 'utf8')
+  res.end(html)
+  return { props: {} }
 }
+```
