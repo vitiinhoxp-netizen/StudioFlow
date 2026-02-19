3ced10666cc86979782052b8423147f7a7516b0d
Victor Lucas Moreira Santos <victor.moreira@atakarejo.com.br>
2026-02-19 18:31:26 -0300

    renomear index.html para app.html

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,12 @@
-import fs from 'fs'
-import path from 'path'
-
-export default function Home({ htmlContent }) {
-  return (
-    <>
-      <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
-    </>
-  )
+export default function Home() {
+  return null
 }
 
-export async function getServerSideProps() {
-  const filePath = path.join(process.cwd(), 'public', 'index.html')
-  const html = fs.readFileSync(filePath, 'utf8')
-  return { props: { htmlContent: html } }
+export async function getServerSideProps({ res }) {
+  const fs = require('fs')
+  const path = require('path')
+  const html = fs.readFileSync(path.join(process.cwd(), 'public', 'app.html'), 'utf8')
+  res.setHeader('Content-Type', 'text/html; charset=utf-8')
+  res.end(html)
+  return { props: {} }
 }
