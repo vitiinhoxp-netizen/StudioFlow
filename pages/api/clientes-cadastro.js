1345dc68e4602d10ad0b031e1f9067ae7aa3f201
Victor Lucas Moreira Santos <victor.moreira@atakarejo.com.br>
2026-02-26 11:37:28 -0300

    corrigir rotas api clientes

--- pages/api/clientes-cadastro.js
+++ pages/api/clientes-cadastro.js
@@ -2,8 +2,8 @@ import { createClient } from '@supabase/supabase-js'
 import bcrypt from 'bcryptjs'
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 )
 
 export default async function handler(req, res) {
